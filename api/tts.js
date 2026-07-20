f0342827dac0c897f826ddb5ebb0b9b394a73ac2
رنيم فاي <raniimfey@gmail.com>
2026-07-20 03:26:59 +0100

    Update tts.js

--- api/tts.js
+++ api/tts.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  export default async function handler(req, res) {
   // تشخيص مؤقت
   if (req.method === 'GET') {
     return res.status(200).json({ 
